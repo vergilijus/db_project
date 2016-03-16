cb620bcc98afa26129fb1a9e7645709f69980303
Ruslan <vergilijus@ya.ru>
2016-03-16 15:16:39 +0300

    Заготовки схемы бд.

--- schema.sql
+++ schema.sql
@@ -1,1 +1,30 @@
-SELECT * FROM
+DROP TABLE IF EXISTS Forum;
+DROP TABLE IF EXISTS Post;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Thread;
+
+CREATE TABLE Forum (
+  id         INT AUTO_INCREMENT NOT NULL,
+  name       VARCHAR(100)       NOT NULL,
+  short_name VARCHAR(50),
+  user       VARCHAR(50),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Post (
+
+  id     INT AUTO_INCREMENT NOT NULL,
+  date   DATE,
+  thread INT
+  #   todo
+);
+
+CREATE TABLE User (
+  id INT AUTO_INCREMENT NOT NULL
+  #   todo
+);
+
+CREATE TABLE Thread (
+  id INT AUTO_INCREMENT NOT NULL
+  #   todo
+);
