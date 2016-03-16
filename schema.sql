2c45c278c6fb53bb7f1f79f5a2e394c73327a403
Ruslan <vergilijus@ya.ru>
2016-03-16 15:21:11 +0300

    Доработки схемы бд.

--- schema.sql
+++ schema.sql
@@ -6,25 +6,28 @@ DROP TABLE IF EXISTS Thread;
 CREATE TABLE Forum (
   id         INT AUTO_INCREMENT NOT NULL,
   name       VARCHAR(100)       NOT NULL,
-  short_name VARCHAR(50),
-  user       VARCHAR(50),
+  short_name VARCHAR(50)        NOT NULL,
+  user       VARCHAR(50)        NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE Post (
 
   id     INT AUTO_INCREMENT NOT NULL,
-  date   DATE,
-  thread INT
+  date   DATE               NOT NULL,
+  thread INT                NOT NULL, #todo: foreign key
+  PRIMARY KEY (id)
   #   todo
 );
 
 CREATE TABLE User (
-  id INT AUTO_INCREMENT NOT NULL
+  id INT AUTO_INCREMENT NOT NULL,
+  PRIMARY KEY (id)
   #   todo
 );
 
 CREATE TABLE Thread (
-  id INT AUTO_INCREMENT NOT NULL
+  id INT AUTO_INCREMENT NOT NULL,
+  PRIMARY KEY (id)
   #   todo
 );
