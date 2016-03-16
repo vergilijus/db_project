5ea675eecca03cc49d11f37b50719b3854d7f49b
Ruslan <vergilijus@ya.ru>
2016-03-16 19:20:02 +0300

    ---

--- application.lua
+++ application.lua
@@ -1,15 +1,31 @@
-#!/usb/bin/env tarantool
+#! /usb/bin/env tarantool
 
-box.cfg{}
+box.cfg {}
 
 local mysql = require('mysql')
-local conn = mysql.connect({host = localhost, user = 'root', password = 'root', db = 'technopark'})
+local conn = mysql.connect({ host = localhost, user = 'root', password = 'root', db = 'technopark' })
 
-local function handler(self)
-    return self:render{ json = conn:execute('select * from Forum;') }
+local function getForum(self)
+    return self:render { json = conn:execute('select * from Forum;') }
+end
+
+local function getPost(self)
+    return self:render { json = conn:execute('select * from Post;') }
+end
+
+local function status(self)
+    return self:render { json = conn:execute('select count(*) from Forum') }
+end
+
+local function test(self)
+    return self:render { text = 'hello' }
 end
 
 local httpd = require('http.server')
+
 local server = httpd.new('*', 8081)
-server:route({ path = '/'  }, handler)
+server:route({ path = '/forum' }, getForum)
+server:route({ path = '/post' }, getPost)
+server:route({ path = '/status' }, status)
+server:route({ path = '/test' }, test)
 server:start()
--- schema.sql
+++ schema.sql
@@ -19,20 +19,24 @@ CREATE TABLE Thread (
   ENGINE = INNODB;
 
 CREATE TABLE Post (
-
-id     INT AUTO_INCREMENT NOT NULL,
-date   DATE               NOT NULL,
-thread INT                NOT NULL,
-PRIMARY KEY (id),
-FOREIGN KEY (thread)
-REFERENCES Thread (id)
-#   todo
+  id     INT AUTO_INCREMENT NOT NULL,
+  date   DATE               NOT NULL,
+  thread INT                NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (thread)
+  REFERENCES Thread (id)
+  #   todo
 )
-ENGINE = INNODB;
+  ENGINE = INNODB;
 
 CREATE TABLE User (
-id INT AUTO_INCREMENT NOT NULL,
-PRIMARY KEY (id)
-#   todo
+  id       INT AUTO_INCREMENT NOT NULL,
+  username VARCHAR(30)        NOT NULL,
+  about    VARCHAR(100)       NOT NULL,
+  name     VARCHAR(50)        NOT NULL,
+  email    VARCHAR(50)        NOT NULL,
+  isAnonymous BOOL DEFAULT FALSE not NULL,
+  PRIMARY KEY (id)
+  #   todo
 );
 
