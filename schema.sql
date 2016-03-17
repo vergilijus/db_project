6aacb3aa6b6515e29e79e6e93f9667d2c34c6f9d
Ruslan <vergilijus@ya.ru>
2016-03-17 18:26:52 +0300

    Доработки схемы.

--- schema.sql
+++ schema.sql
@@ -3,25 +3,48 @@ DROP TABLE IF EXISTS Post;
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Thread;
 
+CREATE TABLE User (
+  username    VARCHAR(32)           NOT NULL,
+  about       VARCHAR(127)          NOT NULL,
+  name        VARCHAR(63)           NOT NULL,
+  email       VARCHAR(63)           NOT NULL,
+  isAnonymous BOOL DEFAULT FALSE    NOT NULL,
+  isDeleted   BOOLEAN DEFAULT FALSE NOT NULL,
+  PRIMARY KEY (email)
+  #   todo
+);
+
 CREATE TABLE Forum (
   id         INT AUTO_INCREMENT NOT NULL,
   name       VARCHAR(100)       NOT NULL,
-  short_name VARCHAR(50)        NOT NULL,
+  short_name VARCHAR(50)        NOT NULL, # todo: походу это primary key
   user       VARCHAR(50)        NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (user)
+  REFERENCES User (email)
 );
 
 CREATE TABLE Thread (
-  id INT AUTO_INCREMENT NOT NULL,
+  id        INT AUTO_INCREMENT NOT NULL,
+  date      DATE               NOT NULL,
+  title     VARCHAR(255)       NOT NULL,
+  forum     VARCHAR(63)        NOT NULL, # todo: foreign key
+  isClosed  BOOLEAN DEFAULT FALSE,
+  isDeleted BOOLEAN DEFAULT FALSE,
+  message   VARCHAR(255),
+  slug      VARCHAR(255),
+  user      VARCHAR(63), # todo: foreign key
   PRIMARY KEY (id)
   #   todo
 )
   ENGINE = INNODB;
 
 CREATE TABLE Post (
-  id     INT AUTO_INCREMENT NOT NULL,
-  date   DATE               NOT NULL,
-  thread INT                NOT NULL,
+  id         INT AUTO_INCREMENT NOT NULL,
+  date       DATE               NOT NULL,
+  thread     INT                NOT NULL,
+  parent     INT, # todo foreign key,
+  isApproved BOOLEAN,
   PRIMARY KEY (id),
   FOREIGN KEY (thread)
   REFERENCES Thread (id)
@@ -29,14 +52,5 @@ CREATE TABLE Post (
 )
   ENGINE = INNODB;
 
-CREATE TABLE User (
-  id       INT AUTO_INCREMENT NOT NULL,
-  username VARCHAR(30)        NOT NULL,
-  about    VARCHAR(100)       NOT NULL,
-  name     VARCHAR(50)        NOT NULL,
-  email    VARCHAR(50)        NOT NULL,
-  isAnonymous BOOL DEFAULT FALSE not NULL,
-  PRIMARY KEY (id)
-  #   todo
-);
+# todo: узнать что делать с дочерними постами удаленного поста
 
