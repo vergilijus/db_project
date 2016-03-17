cb37c506f566d84b46bb94abb13c4176cb059832
Ruslan <vergilijus@ya.ru>
2016-03-17 19:10:21 +0300

    Ответы ошибок.

--- application.lua
+++ application.lua
@@ -1,4 +1,4 @@
-#!      /usb/bin/env tarantool
+#!                      /usb/bin/env tarantool
 box.cfg {}
 
 local BASE_PATH = '/db/api'
@@ -6,12 +6,32 @@ local BASE_PATH = '/db/api'
 local mysql = require('mysql')
 local conn = mysql.connect({ host = localhost, user = 'root', password = 'root', db = 'technopark' })
 
+local RESPONSES = {
+    "запрашиваемый объект не найден",
+    "невалидный запрос",
+    "некоректный запрос",
+    "неизвестная ошибка",
+    "такой юзер уже существует"
+}
+
+local function errorRequest(code)
+    return {
+        code = code,
+        response = RESPONSES[code]
+    }
+end
+
+
 
 
 --------------
 -- Общие.
 --------------
 local function status(req)
+    if req.method ~= 'POST' then
+        return req:render({ json = errorRequest(3) })
+    end
+
     local fc = conn:execute('select count(*) as forum from Forum')[1].forum
     local tc = conn:execute('select count(*) as thread from Thread')[1].thread
     local pc = conn:execute('select count(*) as post from Post')[1].post
@@ -58,7 +78,7 @@ end
 local httpd = require('http.server')
 
 local server = httpd.new('127.0.0.1', 8081)
-server:route({ path = BASE_PATH..'/forum' }, getForum)
-server:route({ path = BASE_PATH..'/post' }, getPost)
-server:route({ path = BASE_PATH..'/status' }, status)
+server:route({ path = BASE_PATH .. '/forum' }, getForum)
+server:route({ path = BASE_PATH .. '/post' }, getPost)
+server:route({ path = BASE_PATH .. '/status' }, status)
 server:start()
